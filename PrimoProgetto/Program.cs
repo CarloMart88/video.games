bd23c1435f4d3e7c9cf018c730660e5dfad2a2a0
CarloMart88 <carlo.martino1988@gmail.com>
2025-10-28 10:43:42 +0100

    first commit

---
+++ PrimoProgetto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    // some example of escapes 
+    Console.WriteLine("Ciao\nCarlo");
+    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!");
+
+    Console.WriteLine("Lui ha detto: \'No\'");
+    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!");
+
+    Console.WriteLine("Carlo ha detto: \"C# è fantastico\"");
+    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!");
+
+    Console.WriteLine("Percorso: C:\\Users\\Carlo");
+    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!");
+
+    Console.WriteLine("Ciao\rCarlo");
+    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!");
+
+    Console.WriteLine("Carlo\b!");
+    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!");
+  }
+}
